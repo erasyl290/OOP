5d9328453e3f1437ebf190ceb6b4226b5819cc2b
erasyl290 <erasyl290@bk.ru>
2023-09-28 20:13:56 +0600

    Added problem3

---
+++ Week1-4/practice1/problem3/Grade.java
@@ -0,0 +1,38 @@
+package problem3;
+
+public class Grade {
+	private String grade;
+
+	Grade() {
+	}
+
+	Grade(int percentage) {
+		if (percentage <= 100 && percentage >= 95) {
+			grade = "A+";
+		} else if (percentage < 95 && percentage >= 90) {
+			grade = "A";
+		} else if (percentage < 90 && percentage >= 85) {
+			grade = "A-";
+		} else if (percentage < 85 && percentage >= 80) {
+			grade = "B+";
+		} else if (percentage < 80 && percentage >= 75) {
+			grade = "B";
+		} else if (percentage < 75 && percentage >= 70) {
+			grade = "B-";
+		} else if (percentage < 70 && percentage >= 65) {
+			grade = "C+";
+		} else if (percentage < 65 && percentage >= 60) {
+			grade = "C";
+		} else if (percentage < 60 && percentage >= 55) {
+			grade = "C-";
+		} else if (percentage < 55 && percentage >= 50) {
+			grade = "F+";
+		} else {
+			grade = "F";
+		}
+	}
+
+	public String toString() {
+		return grade;
+	}
+}
---
+++ Week1-4/practice1/problem3/TestGrade.java
@@ -0,0 +1,19 @@
+package problem3;
+
+import java.util.Scanner;
+
+public class TestGrade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Write your percentage: ");
+		int percentage = in.nextInt();
+		in.close();
+		
+		Grade grade = new Grade(percentage);
+		System.out.println(grade);
+	}
+
+}
